7ba69261c2cbae2f291519758196f9b71adfb19e
HelenYazh <olenka.yazh05@gmail.com>
2024-07-27 17:01:38 +0300

    basic

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import Product from './Product';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
+      <Product
+        name="Tacos With Lemon"
+        imgUrl="https://media.istockphoto.com/id/1413248571/photo/two-tacos-with-ground-beef-and-lime-on-white-background.jpg?b=1&s=612x612&w=0&k=20&c=bD3wycxcec50b_PKDbuLKxUwYcUDTwy5OGXMsLLr6pg="
+        price={12.49}
+      />
+    </div>
+  );
+}
